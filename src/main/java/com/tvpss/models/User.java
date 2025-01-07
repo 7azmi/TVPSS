1c295a9ebc8087e2816f9fbf51945fe4b4494c64
7azmi <a7.h3.04@gmail.com>
2025-01-08 06:17:04 +0800

    User class updated

--- src/main/java/com/tvpss/models/User.java
+++ src/main/java/com/tvpss/models/User.java
@@ -1,9 +1,59 @@
 package com.tvpss.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     private String username;
     private String password;
     private String role;
 
-    // Getters and Setters
+    // Default constructor
+    public User() {}
+
+    // Constructor with fields
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
